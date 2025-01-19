dfd0bb1bf66f48fb64b9a7f2f69493cccd15a063
harshnishad <harsh.nishad_cs21@gla.ac.in>
2025-01-19 15:17:45 +0530

    worked on :Footer Section

--- src/Component/Pages/Homepage.tsx
+++ src/Component/Pages/Homepage.tsx
@@ -4,7 +4,7 @@ import Features from "../Sections/Features";
 import FunctionPage from "../Sections/FunctionPage";
 import HeroSection from "../Sections/HeroSection";
 import NavBar from "../Sections/NavBar";
-import Footer from "../Sections/Fotter";
+import Footer from "../Sections/Footer";
 
 const Homepage = () => {
   return (
