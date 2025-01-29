c3005b4851f827df243470df39c276b92395d764
harshnishad <harsh.nishad_cs21@gla.ac.in>
2025-01-29 23:02:15 +0530

    fixing bugs: CommentSection

--- src/Component/Pages/HomePage/Homepage.tsx
+++ src/Component/Pages/HomePage/Homepage.tsx
@@ -1,15 +1,15 @@
 
-import CommentPage from "../Sections/HomePage/CommentPage";
-import Features from "../Sections/HomePage/Features";
-import FunctionPage from "../Sections/HomePage/FunctionPage";
-import HeroSection from "../Sections/HomePage/HeroSection";
-// import NavBar from "../Sections/HomePage/NavBar";
-import Footer from "../Sections/HomePage/Footer";
+import CommentPage from "../../Sections/HomePage/CommentPage";
+import Features from "../../Sections/HomePage/Features";
+import FunctionPage from "../../Sections/HomePage/FunctionPage";
+import HeroSection from "../../Sections/HomePage/HeroSection";
+import NavBar from "../../Sections/HomePage/NavBar";
+import Footer from "../../Sections/HomePage/Footer";
 
 const Homepage = () => {
   return (
     <>
-      {/* <NavBar /> */}
+      <NavBar />
       <HeroSection />
       <Features />
       <CommentPage />
